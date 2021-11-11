f0ad13ddff3f1c5264ea88b130c50a836aa6d257
Darius Patiejūnas <patiejun@mail.lt>
2021-11-11 11:27:49 +0200

    sieti

---
+++ ClassLibrary4/NewPageND/Page1.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4.NewPageND
+{
+    public class Page1
+    {
+        private static IWebDriver _driver;
+        private double expectedResultSum;
+
+        private IWebDriver _webdriver;
+        private object _FirstInput;
+
+        private IWebElement _inputField => _driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement _button => _driver.FindElement(By.CssSelector("txtAge"));
+        private IWebElement _result => _driver.FindElement(By.Id("display"));
+
+    }
+}
---
+++ ClassLibrary4/NewPageTest/NewPageTest1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4.NewPageTest
+{
+    public class NewPageTest1 
+
+	
+	{
+		private static IWebDriver _driver;
+        private object driver;
+
+        [OneTimeSetUp]
+		public static void SetUp()
+		{
+			_driver = new ChromeDriver();
+			_driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+			WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+			IWebElement popUp = _driver.FindElement(By.Id("at-cv-lightbox-close"));
+			wait.Until(d => popUp.Displayed);
+			popUp.Click();
+		}
+		[OneTimeTearDown]
+		public static void TearDown()
+		{
+			// _driver.Quit();
+		}
+		[Test]
+		public void tS1()
+		{
+            _driver.Navigate().GoToUrl("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+			_driver.Manage().Window.Size = new System.Drawing.Size(1050, 708);
+			_driver.FindElement(By.Id("isAgeSelected")).Click();
+		}
+	}
+}
+
+	
