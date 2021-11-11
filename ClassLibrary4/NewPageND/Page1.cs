4dfaadb975780aff6c7eb1a44f3525ede8fe8a06
Darius Patiejūnas <patiejun@mail.lt>
2021-11-11 12:08:57 +0200

    sieti

--- ClassLibrary4/NewPageND/Page1.cs
+++ ClassLibrary4/NewPageND/Page1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ClassLibrary4.NewPageND
 {
@@ -14,10 +15,15 @@ namespace ClassLibrary4.NewPageND
 
         private IWebDriver _webdriver;
         private object _FirstInput;
+        private object __driver;
 
         private IWebElement _inputField => _driver.FindElement(By.Id("isAgeSelected"));
-        private IWebElement _button => _driver.FindElement(By.CssSelector("txtAge"));
-        private IWebElement _result => _driver.FindElement(By.Id("display"));
+        private IWebElement Button => _driver.FindElement(By.CssSelector("txtAge"));
+        private IWebElement _result => _driver.FindElement(By.Id("display"));        
+        private IWebElement _button => _driver.FindElement(By.CssSelector(".checkbox:nth-child(3) .cb1-element"));
+        private IWebElement _button1 => _driver.FindElement(By.CssSelector(".checkbox:nth-child(4) .cb1-element"));
+        private IWebElement _button2 => _driver.FindElement(By.CssSelector(".checkbox:nth-child(5) .cb1-element"));
+        private IWebElement _button3 => _driver.FindElement(By.CssSelector(".checkbox:nth-child(6) .cb1-element"));
 
     }
 }
