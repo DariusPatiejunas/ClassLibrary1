66fe02ca98bb5b42a375dd80294a4585b0f87a71
Darius Patiejūnas <patiejun@mail.lt>
2021-11-13 17:14:26 +0200

    sieti

---
+++ ClassLibrary6/Baigiamasis.cs
@@ -0,0 +1,23 @@
+﻿using jsreport.Types;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary6
+{
+    public class Baigiamasis
+    {
+        [Test]
+        public static void TestChromeDriver()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Url = "https://www.tax.lt/";
+            chrome.Quit();
+        }
+    }
+}
---
+++ ClassLibrary6/TestAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibrary6
+{
+    internal class TestAttribute : Attribute
+    {
+    }
+}
