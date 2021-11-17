181d4955cdf7d85c9b6d3164459c1a61fb0a7721
Darius Patiejūnas <patiejun@mail.lt>
2021-11-17 12:58:20 +0200

    sieti

---
+++ ClassLibrary4/NewFolder1Page/PVMskaiciuoklePage.cs
@@ -0,0 +1,35 @@
+﻿// 2021-11-17, Darius Patiejunas,PVMsk
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4.NewFolder1Page
+{
+    class PVMskaiciuoklePage
+    {
+        private static IWebDriver _driver;
+
+        private IWebElement _PvmTarifas5 => _driver.FindElement(By.Id("vat_percent"));
+        private IWebElement _skaicius => _driver.FindElement(By.XPath("//option[. = '5%']"));
+        IWebElement _sumabePVM => _driver.FindElement(By.Id("amount_wo_vat"));
+
+        public PVMskaiciuoklePage(IWebDriver webdriver)
+        {
+            _driver = webdriver;
+        }
+
+        public void PvmTarifas5(string vat_percent)
+        {
+            _PvmTarifas5.SendKeys(vat_percent);
+        }
+
+        public void SumabePVM(string amount_wo_vat)
+        {
+            _sumabePVM.Clear();
+            _sumabePVM.SendKeys("500");
+        }
+    }
+}
---
+++ ClassLibrary4/NewFolder2Test/PVMskaiciuokleTest.cs
@@ -0,0 +1,49 @@
+﻿// 2021-11-17, Darius Patiejunas,PVMsk
+using ClassLibrary4.NewFolder1Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4.NewFolder2Test
+{
+    class PVMskaiciuokleTest
+    {
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://www.tax.lt/skaiciuokles/pvm_skaiciuokle");
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            // _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.FindElement(By.ClassName("fc-button-label")).Click();
+            _driver.Manage().Window.Maximize();
+        }
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            //_driver.Quit();
+        }
+        [Test]
+
+        public void TestPVMskaiciuoklePage()
+        {
+            PVMskaiciuoklePage page = new PVMskaiciuoklePage(_driver);
+
+            string skaicius = "5";
+            page.PvmTarifas5(skaicius);            
+            string amount_wo_vat = "500";            
+            page.SumabePVM("500");
+
+
+
+        }
+    }
+}
